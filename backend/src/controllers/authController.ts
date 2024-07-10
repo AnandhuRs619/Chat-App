6727c03ea045ca3195cd62cc9a965dacc998247c
AnandhuRs619 <ananthurs619@gmail.com>
2024-07-11 02:54:02 +0530

    prisma config and setting up the signup

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,13 +1,64 @@
-import { Request,Response } from "express"
+import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+import bcryptjs from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
-export const signup = async(req:Request,res:Response)=>{
-res.send("user signup ....")
-}
-export const login = async(req:Request,res:Response)=>{
-    res.send("user is logged in ....")
+export const signup = async (req: Request, res: Response) => {
+	try {
+		const { fullname, username, password, confirmPassword, gender } = req.body;
 
-}
-export const logout = async(req:Request,res:Response)=>{
-res.send("user is logged out ....")
-}
+		if (!fullname || !username || !password || !confirmPassword || !gender) {
+			return res.status(400).json({ error: "Please fill in all fields" });
+		}
 
+		if (password !== confirmPassword) {
+			return res.status(400).json({ error: "Passwords don't match" });
+		}
+
+		const user = await prisma.user.findUnique({ where: { username } });
+
+		if (user) {
+			return res.status(400).json({ error: "Username already exists" });
+		}
+
+		const salt = await bcryptjs.genSalt(10);
+		const hashedPassword = await bcryptjs.hash(password, salt);
+
+		// https://avatar-placeholder.iran.liara.run/
+		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+		const newUser = await prisma.user.create({
+			data: {
+				fullname,
+				username,
+				password: hashedPassword,
+				gender,
+				profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+			},
+		});
+
+		if (newUser) {
+			// generate token in a sec
+			generateToken(newUser.id, res);
+
+			res.status(201).json({
+				id: newUser.id,
+				fullname: newUser.fullname,
+				username: newUser.username,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+			res.status(400).json({ error: "Invalid user data" });
+		}
+	} catch (error: any) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+export const login = async (req: Request, res: Response) => {
+  res.send("user is logged in ....");
+};
+export const logout = async (req: Request, res: Response) => {
+  res.send("user is logged out ....");
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import authRouthes from './routes/authRoute.js'
 import messageRoutes from './routes/messageRoute.js'
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+app.use(express.json()) // for parsing the data 
 
 app.use('/api/auth',authRouthes)
 app.use('/api/messages',messageRoutes)
