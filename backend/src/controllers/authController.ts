0a582ade05c883732ff4a9ecfc70a638e7e72546
AnandhuRs619 <ananthurs619@gmail.com>
2024-07-11 23:07:07 +0530

    user CRUD is done

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,61 +4,92 @@ import bcryptjs from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req: Request, res: Response) => {
-	try {
-		const { fullname, username, password, confirmPassword, gender } = req.body;
+  try {
+    const { fullname, username, password, confirmPassword, gender } = req.body;
 
-		if (!fullname || !username || !password || !confirmPassword || !gender) {
-			return res.status(400).json({ error: "Please fill in all fields" });
-		}
+    if (!fullname || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill in all fields" });
+    }
 
-		if (password !== confirmPassword) {
-			return res.status(400).json({ error: "Passwords don't match" });
-		}
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
 
-		const user = await prisma.user.findUnique({ where: { username } });
+    const user = await prisma.user.findUnique({ where: { username } });
 
-		if (user) {
-			return res.status(400).json({ error: "Username already exists" });
-		}
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
 
-		const salt = await bcryptjs.genSalt(10);
-		const hashedPassword = await bcryptjs.hash(password, salt);
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
 
-		// https://avatar-placeholder.iran.liara.run/
-		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    // https://avatar-placeholder.iran.liara.run/
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-		const newUser = await prisma.user.create({
-			data: {
-				fullname,
-				username,
-				password: hashedPassword,
-				gender,
-				profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
-			},
-		});
+    const newUser = await prisma.user.create({
+      data: {
+        fullname,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+      },
+    });
 
-		if (newUser) {
-			// generate token in a sec
-			generateToken(newUser.id, res);
+    if (newUser) {
+      // generate token in a sec
+      generateToken(newUser.id, res);
 
-			res.status(201).json({
-				id: newUser.id,
-				fullname: newUser.fullname,
-				username: newUser.username,
-				profilePic: newUser.profilePic,
-			});
-		} else {
-			res.status(400).json({ error: "Invalid user data" });
-		}
-	} catch (error: any) {
-		console.log("Error in signup controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+      res.status(201).json({
+        id: newUser.id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error: any) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 export const login = async (req: Request, res: Response) => {
-  res.send("user is logged in ....");
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ error: "Please fill in all fields" });
+    }
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      return res.status(400).json({ error: "Invaild Credentials" });
+    }
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invaild Credentials" });
+    }
+    generateToken(user.id,res);
+
+    res.status(200).json({
+       id: user.id,
+       fullname : user.fullname,
+       profilePic: user.profilePic, 
+    })
+
+  } catch (error:any) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 export const logout = async (req: Request, res: Response) => {
-  res.send("user is logged out ....");
+try {
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:" User Logged Out Successfully"})
+} catch (error:any) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+
 };
