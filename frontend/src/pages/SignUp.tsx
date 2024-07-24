aac8ae0ef408ddf40c18a899ab381e37612fae0d
AnandhuRs619 <ananthurs619@gmail.com>
2024-07-24 19:10:27 +0530

    ui

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -34,7 +34,7 @@ const SignUp = () => {
 						<label className='label p-2'>
 							<span className='text-base label-text'>Full Name</span>
 						</label>
-						<input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10'
+						<input type='text' placeholder='Full Name' className='w-full input input-bordered  h-10'
 						value={inputs.fullname}
 						onChange={(e)=>setInputs({... inputs,fullname: e.target.value})} />
 					</div>
@@ -43,7 +43,7 @@ const SignUp = () => {
 						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-10'
+						<input type='text' placeholder='UserName' className='w-full input input-bordered h-10'
 						value={inputs.username}
 						onChange={(e)=> setInputs({... inputs,username:e.target.value})} />
 					</div>
